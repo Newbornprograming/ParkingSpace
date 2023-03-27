d1f53f909d61da3b527f5564a719a1ef5f3f8944
newbornprograming <yohandeny.10@gmail.com>
2023-03-27 15:25:34 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import pickle
+
+#Video feed
+cap = cv2.VideoCapture('carPark.mp4')
+
+while True:
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
