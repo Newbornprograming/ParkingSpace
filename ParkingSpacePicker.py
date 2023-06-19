78904849a04dec06a65a4a859970537b85e59d4f
Yohanes Deny <112600471+Newbornprograming@users.noreply.github.com>
2023-06-19 18:20:13 +0700

    Update ParkingSpacePicker.py

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -1,7 +1,7 @@
 import cv2
 import pickle
 
-width, height = 195,370
+width, height = 185, 310
 
 try:
     with open('CarParkPos', 'rb') as f:
@@ -9,25 +9,23 @@ try:
 except:
     posList = []
 
-def mouseClick(events,x,y,flags,params):
+def mouseClick(events, x, y, flags, params):
     if events == cv2.EVENT_LBUTTONDOWN:
         posList.append((x,y))
-        # klik kiri untuk membuat mark
-        # klik kanan untuk menghapus mark
     if events == cv2.EVENT_RBUTTONDOWN:
         for i, pos in enumerate(posList):
-            x1, y1 = pos
+            x1,y1 = pos
             if x1<x<x1+width and y1<y<y1+height:
                 posList.pop(i)
 
-        with open('CarParkPos', 'wb') as f:
-            pickle.dump(posList, f)
+    with open('CarParkPos', 'wb') as f:
+        pickle.dump(posList, f)
 
 while True:
-    img = cv2.imread('carParkImg.png')
+    img = cv2.imread('carParkImg15.png')
     for pos in posList:
-        cv2.rectangle(img, pos, (pos[0]+width,pos[1]+height), (255,0,255), 2)
-        # cv2.rectangle(img, (720,420), (910,783), (255,0,255), 2)
-    cv2.imshow("Image", img)
+        cv2.rectangle(img, pos,(pos[0]+width,pos[1]+height),(255,0,255),2)
+    # cv2.rectangle(img, (0,100),(110,280),(255,0,255),2)
+    cv2.imshow("Image",img)
     cv2.setMouseCallback("Image",mouseClick)
     cv2.waitKey(1)
